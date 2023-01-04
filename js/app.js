38a7aa36eddde64c0583c1e30df0eaaabd77c62e
srdjan <srdjan.gr@gmail.com>
2023-01-04 22:59:26 +0100

    Creating local storage Array with IMDBID for using later in Selected Movies list. Query for full movie details

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
-import keys from './auth.js'
+// import keys from './auth.js'
 
 const resaultContainer = document.querySelector('#resaultContainer');
 // const searchInput = document.querySelector('#searchInput');
 const searchBtn = document.querySelector('#searchBtn');
 
 const searchInput = document.getElementById('searchInput')
+const listBtn = document.getElementById('listBtn')
+const selectedList = document.getElementById('selectedList')
+const singleElement = document.getElementById('singleElement')
 
+const key = 'b1c7adef';
 let page = 1;
 
+let movies = []
+let moviesList = []
 
 // Pretraga na klik Search dugmeta
 searchBtn.addEventListener('click', async () => {
@@ -15,43 +21,101 @@ searchBtn.addEventListener('click', async () => {
     // searchInput.value = '';
     resaultContainer.innerHTML = '';
 
-    const url = `https://www.omdbapi.com/?apikey=${keys.key}&s=${searchInput.value}&page=${page}`;
+    // const url = `https://www.omdbapi.com/?apikey=${keys.key}&s=${searchInput.value}&page=${page}`;
+    const url = `https://www.omdbapi.com/?apikey=${key}&s=${searchInput.value}&page=${page}`;
 
     try {
         await fetch(url)
             .then((response) => response.json())
             .then((data) => {
-                showSearcResault(data.Search)
-        });
-        
+
+                movies = data.Search
+                showSearcResault()
+            });
+
     } catch (error) {
         console.log(error);
     }
 })
 
 
+// Pretraga na Enter dugme
+searchInput.addEventListener('keypress', async (e) => {
+
+    if(e.key === 'Enter'){
+        searchInput.value = '';
+        resaultContainer.innerHTML = '';
+    
+        const url = `https://www.omdbapi.com/?apikey=${key}&s=${searchInput.value}&page=${page}`;
+    
+        try {
+            await fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+    
+                    movies = data.Search
+
+                    showSearcResault()
+                });
+    
+        } catch (error) {
+            console.log(error);
+        }
+    }
+})
+
+
 
 // Prikazivanje rezultata pretrage 
-const showSearcResault = (data) => {
+const showSearcResault = () => {
 
-    data.forEach(element => {
+    movies.forEach((element, idx) => {
+
+        // console.log(element)
+
         resaultContainer.innerHTML += `
-            <div class="col-3 d-flex flex-wrap justify-content-center my-3 ">
-                <div class="card text-bg-dark customOpacity " >
-                    <img src=${element.Poster} class="card-img-top imgHeight" alt="..." />
+            <div class="col d-flex justify-content-center my-3 ">
+                <div class="card text-bg-dark customOpacity " style="width: 19rem;">
+                    <img src=${element.Poster} class="card-img-top imgHeight" alt="..." style="height: 22rem; object-fit: cover; "/>
                     <div class="card-body py-2">
-                        <h5 class="card-title m-0 text-white-50">${element.Title}</h5>
+                        <h5 class="card-title m-0 text-white-50" >${element.Title}</h5>
                     </div>
-                    <ul class="list-group list-group-flush border-dark">
+                    <ul class="list-group list-group-flush border-dark" >
                         <li class="list-group-item text-bg-dark fs-6 border-dark text-white-50">Year: ${element.Year}</li>
                         <li class="list-group-item text-bg-dark fs-6 border-dark text-white-50">Genre: ${element.Type}</li>
                     </ul>
                     <div class="card-body d-flex justify-content-between p-3 px-3 border-dark">
-                        <a href="#" class="card-link text-info fs-6">More Info</a>
-                        <button class='btn btn-outline-success btn-sm'>Add to list</button>
+                        <a href="#" class="card-link text-info fs-6" onclick=detailInfo('${element.imdbID}') >More Info</a>
+                        <span class="btn btn-outline-success btn-sm" onclick=addToList('${element.imdbID}') >Add to list</span>
                     </div>
                 </div>
             </div>
-        `
-    });
+        `;
+    })
+};
+
+
+// Dodavanje u LocalStorage IMDBID-jeva za koriscenje u Listi Odabranih fiolmova
+const addToList = (imdbID) => {
+
+    moviesList.push(imdbID);
+    localStorage.setItem('movieList', moviesList);
 }
+
+
+// Detaljne onformacije o filmu sa slanje zahteva sa IMDBID 
+const detailInfo = async (imdbID)  => {
+
+    const url = `https://www.omdbapi.com/?apikey=${key}&i=${imdbID}`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+
+            console.log(data)
+        });
+
+}
+
+
+
